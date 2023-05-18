5573c4dd109258d0132288189cfdfbf08a4db735
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-05-18 13:35:09 -0300

    create schema user and endpoint do create new user

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from fastapi import status
+
+from app.models.user import User
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World (qualquer coisa)"}
+    return {"Hello": "Projeto do kanboom-api (studywise)"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.post("/user", status_code=status.HTTP_201_CREATED)
+async def create_user(user: User):
+    del user.id
+    return user
